ba68860647d7fe1812cc6e8c73fd09c71210030f
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2020-10-06 09:54:09 -0400

    Updated PROS Kernel to 3.3.0

--- src/control/auton/chassisAuton.cpp
+++ src/control/auton/chassisAuton.cpp
@@ -39,3 +39,30 @@ double correctionThreshold;
 //rectCoord relTarget;
 rectCoord absTarget;
 rectCoord lastTarget;
+
+
+void translate(double targetDistance, double targetTheta, double targetOrientation, int maxSpeed){
+
+  if(targetOrientation < 0)
+    turnDirection = -1;
+  else
+    turnDirection = 1;
+
+  // newAngle = targetOrientation + position.angle;  //use if you figure out a way to run this only once
+
+  angleToTarget = targetOrientation - getAngleDeg();
+
+  voltageY = PIDloop(0.75, 0.0, 0.5, cos(-(targetTheta + getAngleDeg()))*targetDistance, position.yPosition);
+  voltageX = PIDloop(0.75, 0.0, 0.5, sin(-(targetTheta + getAngleDeg()))*targetDistance, position.xPosition);
+  voltageR = PIDloop(0.75, 0.0, 0.5, targetOrientation, getAngleDeg());
+
+  if(abs(voltageY) > maxSpeed)
+    voltageY = maxSpeed;
+  if(abs(voltageX) > maxSpeed)
+    voltageX = maxSpeed;
+  if(abs(voltageR) > maxSpeed)
+    voltageR = maxSpeed;
+
+  setDrive(voltageY, voltageX, voltageR);
+
+}
